37d3ef25db991dc3677d6b972c04fea0aa38e18d
Chris <chris@postfmly.com>
2026-01-16 02:27:30 -0600

    feat: add/edit/delete

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# TODO
+
+- [ ] Fix option select
--- frontend/src/components/display/index.tsx
+++ frontend/src/components/display/index.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState, type MouseEvent } from "react"
 
 import {
   MinusIcon,
+  PencilIcon,
   PlayCircleIcon,
   PlusCircleIcon,
   PlusIcon,
+  TrashIcon,
   XCircleIcon
 } from "@heroicons/react/24/outline"
 import DataTable, {
@@ -40,6 +42,7 @@ export default function Display() {
   const [expandAll, setExpandAll] = useState(false)
   const [showAdd, setShowAdd] = useState(true)
   const [showCancel, setShowCancel] = useState(false)
+  const [editing, setEditing] = useState<IMeal | null>(null)
 
   const {
     formState: { errors },
@@ -49,19 +52,127 @@ export default function Display() {
   } = useForm<IMeal>()
 
   const onSubmit: SubmitHandler<IMeal> = (meal: IMeal) => {
-    handleClick()
-    // TODO
-    console.log(meal)
+    if (editing) {
+      fetch(API_URL + "/api/update/" + meal.id, {
+        body: JSON.stringify(meal),
+        method: "PUT"
+      })
+        .then((response: Response) => {
+          if (!response.ok) {
+            throw new Error(`Status: ${response.status}`)
+          }
+          return response.json()
+        })
+        .then((meal: IMeal) => {
+          if (!meal) {
+            throw new Error(`Error editing meal: ${meal}`)
+          }
+          location.reload()
+        })
+        .catch((e: Error) => {
+          console.error(e)
+        })
+    } else {
+      fetch(API_URL + "/api/add/", {
+        body: JSON.stringify(meal),
+        method: "POST"
+      })
+        .then((response: Response) => {
+          if (!response.ok) {
+            throw new Error(`Status: ${response.status}`)
+          }
+          return response.json()
+        })
+        .then((meal: IMeal) => {
+          if (!meal) {
+            throw new Error(`Error adding meal: ${meal}`)
+          }
+          location.reload()
+        })
+        .catch((e: Error) => {
+          console.error(e)
+        })
+    }
+  }
+
+  function handleEdit(e: MouseEvent<SVGSVGElement>) {
+    const id = (e.target as SVGElement).dataset.id
+    fetch(API_URL + "/api/get/" + id)
+      .then((response: Response) => {
+        if (!response.ok) {
+          throw new Error(`Status: ${response.status}`)
+        }
+        return response.json()
+      })
+      .then((meal: IMeal) => {
+        if (!meal) {
+          throw new Error(`Error getting id: ${id}`)
+        }
+        setEditing(meal)
+        setShowAdd(false)
+        setShowCancel(true)
+      })
+      .catch((e: Error) => {
+        console.error(e)
+      })
+  }
+
+  function handleTrash(e: MouseEvent<SVGSVGElement>) {
+    const id = (e.target as SVGElement).dataset.id
+    fetch(API_URL + "/api/delete/" + id, {
+      method: "DELETE"
+    })
+      .then((response: Response) => {
+        if (!response.ok) {
+          throw new Error(`Status: ${response.status}`)
+        }
+        return response.json()
+      })
+      .then((result: boolean) => {
+        if (!result) {
+          throw new Error(`Error deleting id: ${id}`)
+        }
+        location.reload()
+      })
+      .catch((e: Error) => {
+        console.error(e)
+      })
   }
 
   const cols: TableColumn<IMeal>[] = [
     {
-      selector: (col: IMeal) => days[col.day],
+      selector: (meal: IMeal) => days[meal.day],
       width: "120px"
     },
     {
-      selector: (col: IMeal) => col.title,
+      selector: (meal: IMeal) => meal.title,
       wrap: true
+    },
+    {
+      button: true,
+      cell: (meal: IMeal) => (
+        <PencilIcon
+          className="size-4 text-yellow cursor-pointer"
+          data-id={meal.id}
+          onClick={handleEdit}
+          title="Edit"
+        />
+      ),
+      ignoreRowClick: true,
+      width: "30px"
+    },
+    {
+      button: true,
+      cell: (meal: IMeal) => (
+        <TrashIcon
+          className="size-4 text-yellow cursor-pointer"
+          data-id={meal.id}
+          onClick={handleTrash}
+          title="Remove"
+        />
+      ),
+      ignoreRowClick: true,
+      width: "30px"
     }
   ]
 
@@ -125,6 +236,8 @@ export default function Display() {
     } else {
       setShowAdd(true)
       setShowCancel(false)
+      setEditing(null)
+      reset()
     }
   }
 
@@ -145,13 +258,13 @@ export default function Display() {
         }
         return response.json()
       })
-      .then((menus: IMeal[]) => {
-        menus.forEach((menu: IMeal) => {
-          if (menu.description.length === 0) {
-            menu.disabled = true
+      .then((meals: IMeal[]) => {
+        meals.forEach((meal: IMeal) => {
+          if (meal.description.length === 0) {
+            meal.disabled = true
           }
         })
-        setData(menus)
+        setData(meals)
       })
       .catch((e: Error) => {
         console.error(e)
@@ -161,7 +274,7 @@ export default function Display() {
 
   return (
     <>
-      <div className="text-center mt-10 size-fit mx-auto px-1">
+      <div className="text-center mt-10 mx-auto px-1 max-w-lg">
         {data.length > 0 ? (
           <span className="float-end mb-1 mr-1 text-yellow2">
             {expandAll ? (
@@ -232,15 +345,22 @@ export default function Display() {
               className="border-1 text-yellow2 border-yellow rounded-md px-3 py-1.5 mr-3 mb-3"
               style={
                 errors.day && { border: "3px double #932c04" /* text-red */ }
-              }>
-              <option key="0" value="">
+              }
+              value={editing ? days[editing.day] : ""}>
+              <option key="0" value="" className="bg-red2 font-bold">
                 Choose a day...
               </option>
-              {days.map((day: string, i: number) => (
-                <option className="bg-red2" key={i} value={day}>
-                  {day}
-                </option>
-              ))}
+              {days
+                .filter((day: string) => {
+                  return editing && days[editing.day] === day
+                    ? day
+                    : !data.find((meal: IMeal) => days[meal.day] === day)
+                })
+                .map((day: string, i: number) => (
+                  <option className="bg-red2" key={i} value={day}>
+                    {day}
+                  </option>
+                ))}
             </select>
             <input
               {...register("title", { required: true })}
@@ -252,6 +372,7 @@ export default function Display() {
               }
               title="Enter meal title..."
               type="text"
+              value={editing?.title}
             />
             <textarea
               {...register("description")}
@@ -260,6 +381,7 @@ export default function Display() {
               placeholder="Enter meal description..."
               rows={2}
               title="Enter meal description..."
+              value={editing?.description}
             />
             <button
               className="cursor-pointer border-1 rounded-md border-yellow px-2 py-1 text-yellow2 inline mb-3"
