5ae8609af08008cf527cf4275d95e6ebff5ffc28
Chris <chris@postfmly.com>
2026-01-20 23:36:26 -0600

    build: using simpleHtml plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,21 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import versionPlugin from "vite-plugin-package-version"
+import version from "vite-plugin-package-version"
+import simpleHtml from "vite-plugin-simple-html"
 import webFontDownload from "vite-plugin-webfont-dl"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
   plugins: [
     react(),
+    simpleHtml({
+      inject: {
+        data: {
+          title: "Planrr"
+        }
+      },
+      minify: true
+    }),
     tailwindcss(),
     webFontDownload(
       [
@@ -15,7 +24,7 @@ export default defineConfig({
       ],
       { assetsSubfolder: "fonts", injectAsStyleTag: false }
     ),
-    versionPlugin()
+    version()
   ],
   test: {
     environment: "jsdom",
